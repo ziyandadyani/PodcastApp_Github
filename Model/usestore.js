c6108b257c5b31936aa47147dec447d408ae0dd2
Ziyanda Dyani <106772674+ziyandadyani@users.noreply.github.com>
2023-08-08 06:48:31 +0200

    Delete Model directory

--- Model/usestore.js
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-const useLoadingStore = create((set) => ({
-  loading: true,
-  setLoading: (isLoading) => set({ loading: isLoading }),
-}));
-
-export default useLoadingStore;
